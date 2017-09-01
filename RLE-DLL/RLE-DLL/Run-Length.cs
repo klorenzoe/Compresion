9867933b555431f71d3d237c99773548d805c0cb
jasgs <jasgs@DESKTOP-FT1HJ5N>
2017-09-01 13:55:10 -0600

    Solucion de la DLL creada -Esta es un proyecto de consola que posteriormente se convertira en proyecto de biblioteca de clases para crear la DLL del algoritmo RLE. -La aplicacion real del algoritmo se creará en una solucion de proyecto aparte en donde se añadira la DLL y dicha solucion sera una aplicacion de consola.

---
+++ RLE-DLL/RLE-DLL/Run-Length.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RLE_DLL
+{
+    class Run_Length
+    {
+        /// Esta solucion de visual es solo para probar la DLL
+        /// Aqui trabajaremos la compresion RLE o Run-Length, es una aplicacion
+        /// de consola pues es mas facil depurar el codigo.
+        /// La aplicacion real la creará Kathy en la que ella trabajara
+        /// Esta clase luego la copiaremos a una solucion de biblioteca de clases o DLL
+        /// 
+
+    }
+}
